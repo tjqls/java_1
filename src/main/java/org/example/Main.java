0dcfcfe13598eda81a09f2b2c2d4d35a910eb4e8
tjqls <tjqls2013@gmail.com>
2023-03-08 12:26:34 +0900

    java2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,41 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!111");
+        //System.out.println("Hello world!111");
+        //System.out.println("안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요");
+
+        int x = 10;
+        //System.out.println(x);
+        //System.out.println("x");
+        //System.out.println("x"+x);
+        //System.out.println(x+x);
+        //System.out.println("");
+        //System.out.println(x + 2);
+        //System.out.println(x - 2);
+        //System.out.println(x * 2);
+        //System.out.println(x / 2);
+        //System.out.println(x % 2);
+        int age = 21;
+
+        System.out.println("당신의 나이 " + age);
+
+        if(age>=20 && age<=34){
+            System.out.println("청년입니다.");
+        }else{
+            System.out.println("청년이 아닙니다.");
+        }
+
     }
 }
+
+//자바스크립트 = 인터프리터 언어
+//자바 = 컴파일 언어
+//자바 스크립트 console.log() = 자바 System.out.println()
+//개행 = \n
+
+//int a=1;
+//a=2;
+
+
+
+
