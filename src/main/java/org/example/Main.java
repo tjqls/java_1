402d8452d04de7beeb8db1458f5ae6d9184976c0
tjqls <tjqls2013@gmail.com>
2023-03-13 13:25:04 +0900

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,29 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        //System.out.println("Hello world!111");
-        //System.out.println("안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요");
 
-        int x = 10;
-        //System.out.println(x);
-        //System.out.println("x");
-        //System.out.println("x"+x);
-        //System.out.println(x+x);
-        //System.out.println("");
-        //System.out.println(x + 2);
-        //System.out.println(x - 2);
-        //System.out.println(x * 2);
-        //System.out.println(x / 2);
-        //System.out.println(x % 2);
-        int age = 21;
-
-        System.out.println("당신의 나이 " + age);
-
-        if(age>=20 && age<=34){
-            System.out.println("청년입니다.");
-        }else{
-            System.out.println("청년이 아닙니다.");
-        }
 
     }
 }
